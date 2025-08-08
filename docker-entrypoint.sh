587a2256821a8428c5b11412716c3fd85c89ba52
tjx <2386584329@qq.com>
2025-08-08 11:27:45 +0800

    perf：去除多余文件

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 默认值，如果未提供环境变量 BASE_URL
-BASE_URL=${BASE_URL:-http://47.109.151.83:9090}
+BASE_URL=${BASE_URL:-http://47.109.151.83:9999}
 
 echo "🔧 Injecting BASE_URL=$BASE_URL into config.json..."
 sed -i "s|__BASE_URL__|$BASE_URL|g" /usr/share/nginx/html/config.json
